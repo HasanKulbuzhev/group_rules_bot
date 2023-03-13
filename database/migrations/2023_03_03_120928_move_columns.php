ca0ff14aa4b51898f36d69f725133812fcf52ce4
HasanKulbuzhev <ashatkfhcj@gmail.com>
2023-03-14 00:58:34 +0300

    fix

--- database/migrations/2023_03_03_120928_move_columns.php
+++ database/migrations/2023_03_03_120928_move_columns.php
@@ -14,7 +14,7 @@ class MoveColumns extends Migration
     public function up()
     {
         Schema::table('tags', function (Blueprint $table) {
-            $table->string('name')->unique(false)->nullable()->change();
+            $table->dropUnique('tags_name_unique');
         });
     }
 
@@ -25,8 +25,5 @@ class MoveColumns extends Migration
      */
     public function down()
     {
-        Schema::table('tags', function (Blueprint $table) {
-            $table->string('name')->nullable()->change();
-        });
     }
 }
