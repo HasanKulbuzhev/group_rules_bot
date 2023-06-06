f1c52a2186b92adf16db139fbd5957f0f6f3a91c
HasanKulbuzhev <ashatkfhcj@gmail.com>
2023-06-06 16:58:37 +0300

    add moon calculation route name

--- app/Http/Requests/TelegramUpdateRequest.php
+++ app/Http/Requests/TelegramUpdateRequest.php
@@ -14,11 +14,11 @@ class TelegramUpdateRequest extends FormRequest
     public static function rule(): array
     {
         return [
-            'message' => ['required_without:callback_query', 'array'],
-            'message.from' => ['required_with:message', 'array'],
-            'message.chat' => ['required_with:message', 'array'],
-            'callback_query' => ['array', 'required_without:message'],
-            'callback_query.message' => ['required_with:callback_query', 'array'],
+//            'message' => ['required_without:callback_query', 'array'],
+//            'message.from' => ['required_with:message', 'array'],
+//            'message.chat' => ['required_with:message', 'array'],
+//            'callback_query' => ['array', 'required_without:message'],
+//            'callback_query.message' => ['required_with:callback_query', 'array'],
         ];
     }
 }
