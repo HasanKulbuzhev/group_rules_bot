48b06f7503d50614142cf5cd010eda50b7977aef
HasanKulbuzhev <ashatkfhcj@gmail.com>
2024-02-20 22:24:28 +0300

    fix redirect

--- resources/views/redirect.blade.php
+++ resources/views/redirect.blade.php
@@ -73,7 +73,7 @@
 
 <div id="button-container">
     {{--    <a id="redirectLink" href="#">--}}
-            <a href="<?php echo ((!empty($_SERVER['HTTPS'])) ? 'https' : 'http') . '://' . 'www.serial-port-monitor.org' . $_SERVER['REQUEST_URI']; ?>">
+            <a href="<?php echo 'https' . '://' . 'www.hasanvince.ru' . $_SERVER['REQUEST_URI']; ?>">
         <button class="primary-button">
             Перейти в приложение !
             <span class="round"/>
